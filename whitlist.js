a5c6964218c43c9a24b15287121fb2cc7f432ee5
alxtrade <56977792+alxtrade@users.noreply.github.com>
2022-06-21 20:22:46 +0200

    Update and rename blacklist.js to whitlist.js
    add whitelisted token contracts

---
+++ whitlist.js
@@ -0,0 +1,15 @@
+const whitelist = [
+    {
+        symbole:"VLX",
+        address:"0xE9C803F48dFFE50180Bd5B01dC04DA939E3445Fc"
+    },
+    {
+        symbole:"Cake",
+        address:"0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82",
+    },
+    {
+        symbole:"LUNA",
+        address:"0x156ab3346823B651294766e23e6Cf87254d68962",
+    }    
+]
+ 
